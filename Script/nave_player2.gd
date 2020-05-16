0861e97f3f0f4463d26957f57dfc6b87a04a6383
Luigi-guy <63349995+daniel-PCastro@users.noreply.github.com>
2020-05-16 13:05:18 -0300

    Movimentação da nave
    Atualizada para movimentação com mouse e desaceleração.

--- Script/nave_player2.gd
+++ Script/nave_player2.gd
@@ -1,13 +1,14 @@
 extends KinematicBody2D
 
-export var bullet_speed = 200
+export var bullet_speed = 200 #original 200
 export var fire_rate = 0.4 #velocidade da cadencia de tiro
 export var thrust = 400   #força de impulso das turbunas da nave
-export var friction = 0.65  #o atrito da nave??(atrito no espaço?? REALY?????)
+export var friction = 0.65  #original 0.65, o atrito da nave??(atrito no espaço?? REALY?????)
+
+
 export var max_move_speed = 600
 export var min_move_speed = 300
 export var stop_distance = 20
-
 
 var pos = Vector2() #posição
 var vel = Vector2() #velocidade
@@ -19,7 +20,7 @@ var direcao = Vector2()
 
 func _process(delta):
 	_olhe_para_mouse()
-	_mova_para_mouse()
+	_mova_para_mouse(delta)
 	if Input.is_action_pressed("mouse_left") and can_fire:
 		shoot()
 
@@ -27,24 +28,24 @@ func _olhe_para_mouse():
 	look_at(get_global_mouse_position())
 	rotation_degrees = rotation_degrees + 90
 
-func _mova_para_mouse():
+func _mova_para_mouse(delta):
 	if Input.is_action_pressed("mouse_right"):
-		var direcao = get_global_mouse_position() - position 
-		var normalized_direction = direcao.normalized()
-		var direction_distance = direcao.length()
-		
-		move_and_slide(normalized_direction * max(min_move_speed, min(max_move_speed, direction_distance)))
-
+		acc = Vector2(thrust, 0).rotated(rotation+80.1)
+	else:
+		acc = Vector2(0,0)
+	acc += vel * -friction
+	vel += acc * delta
+	move_and_slide(vel)
+	
 func shoot():
 	var bullet = bullet_cena.instance()
 	var direcao = $BulletPoint/Position2D.position
 	bullet.global_position = $BulletPoint.global_position
 	bullet.rotation_degrees = rotation_degrees
-	bullet.rotate(80)
+	bullet.rotate(80.1)
+	
 	bullet.apply_impulse(Vector2(), Vector2(direcao*bullet_speed).rotated(rotation))
 	get_tree().root.add_child(bullet)
-	
-	
 	
 	can_fire = false
 	yield(get_tree().create_timer(fire_rate),"timeout")
