ed9f7ac90a0239a63233910207da7c9043f771f0
Renan Augusto Ferreira Soares <rennafs@gmail.com>
2020-05-17 21:02:46 -0300

    Add files via upload

--- script/nave_player2.gd
+++ script/nave_player2.gd
@@ -8,16 +8,33 @@ export var friction = 0.65  #original 0.65, o atrito da nave??(atrito no espaço
 var vel = Vector2() #velocidade
 var acc = Vector2() #aceleração
 
+#Gema
+var ghost_gem = preload("res://Cenas/ghost_gem.tscn") 
+var gem_droped 
+
 var bullet_cena = preload("res://Cenas/player_bullet.tscn")
 var can_fire = true
 var direcao = Vector2()
 
+#Renan: função para setar a gema como invisivel
+func _ready():
+	$gem_target.visible = false
+	
 func _process(delta):
 	_olhe_para_mouse()
 	_mova_para_mouse(delta)
 	if Input.is_action_pressed("mouse_left") and can_fire:
 		shoot()
-
+	#Renan: Solta a Gema, trocar pela morte depois
+	if Input.is_action_just_pressed("ui_z"):
+		morte_falsa()
+	#Renan: Oculta ou mostra a gema ao lado da barra de vida
+	var gem_target = $"/root/global".collected
+	if(gem_target==true):
+		$gem_target.visible = true
+	else:
+		$gem_target.visible = false
+		
 func _olhe_para_mouse():
 	look_at(get_global_mouse_position())
 	rotation_degrees = rotation_degrees + 90
@@ -47,3 +64,14 @@ func shoot():
 
 func recebendo_dano():
 	$ProgressBar.value -= 20
+
+func morte_falsa():
+	#instancia a gema com base na posição atual do player
+		var gem = ghost_gem.instance()
+		#gem.global_position = $ghost_gem.global_position
+		get_tree().root.add_child(gem)
+		#Oculta as indicações da bandeira
+		gem_droped = false
+		$"/root/global".collected_gem(gem_droped)
+		
+		
