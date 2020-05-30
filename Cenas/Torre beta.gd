42452ae5b0f41e009629491dc75525cad8f786d1
HenriqueCCF20 <65455626+HenriqueCCF20@users.noreply.github.com>
2020-05-29 22:55:34 -0300

    Mira da torre corrigida

--- Cenas/Torre beta.gd
+++ Cenas/Torre beta.gd
@@ -1,6 +1,10 @@
 extends Node2D
 
-var bullet_speed = 2
+onready var player = get_parent().get_node("player2")
+
+var player_position
+var angulo
+var bullet_speed = 200
 var can_fire = true
 var bullet_cena = preload("res://Cenas/Torret fire.tscn")
 var fire_rate = 0.4
@@ -18,15 +22,18 @@ func _on_Detec_body_shape_entered(body_id, body, body_shape, area_shape):
 	var nomebody = body.get_name()
 	#Checa o nome do corpo
 	if "player2" == nomebody:
+		player_position = player.get_global_position()
 		var bullet = bullet_cena.instance()
-		var direcao = get_parent().get_node("player2").get_transform()
-		direcao = direcao[2]
-		print(direcao)
+		var direcao = $BulletPoint.position
+		angulo = (get_angle_to(player_position)/3.14*180)
+		get_node("BulletPoint").rotation_degrees = (angulo)
+		print(angulo)
+		print(get_node("BulletPoint").rotation_degrees)
 		
-		bullet.global_position = $BulletPoint.global_position
-		bullet.rotation_degrees = rotation_degrees
-		bullet.rotate(80)
-		bullet.apply_impulse(Vector2(), Vector2(direcao*bullet_speed).rotated(rotation))
+		bullet.global_position = $BulletPoint/Bullet.global_position
+		
+		bullet.rotation = get_angle_to(player_position)
+		bullet.apply_impulse(Vector2(), Vector2(direcao*bullet_speed))
 		get_tree().root.add_child(bullet)
 	
 		can_fire = false
