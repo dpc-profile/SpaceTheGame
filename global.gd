ff093ae897387de9a002913612897bebca024188
Renan Augusto Ferreira Soares <rennafs@gmail.com>
2020-06-01 10:19:26 -0300

    Add files via upload

--- global.gd
+++ global.gd
@@ -3,15 +3,15 @@ extends Node
 var collected 
 var status
 var gem = "die"
+var pickable_gem
 
-func _ready():
-	 pass
-
+#determina se a gema foi coletada
 func collected_gem(gem_collected):
 	collected = gem_collected
 	
 #Faz o Kinematic Body sumir
 func kill_gem_body():
 	 status = gem
-	
-	
+
+func ellegible_pickable(pickable):
+	pickable_gem = pickable
