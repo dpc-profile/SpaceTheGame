09eebb264fdebc3f33128a464b48ee159168b86a
Luigi-guy <63349995+daniel-PCastro@users.noreply.github.com>
2020-06-10 19:23:13 -0300

    Adaptando pro multiplayer
    -Menu modificado para escolher entre Single player e multiplayer
    -Script base de network adicionado mas não implementado

--- Cenas/TelaMenu/TitleScream.gd
+++ Cenas/TelaMenu/TitleScream.gd
@@ -1,8 +1,15 @@
 extends Control
+
 #Chama a chama a MusicaDoMenu
 func _ready():
 	get_node("/root/menu_musica/")
-	pass
+	
+#Esconde a HBox do menu inicial e mostra a HBox de escolha modo Solo ou multiplayer	
+func _on_NewGameButton_pressed():
+	$MainChoices.hide()
+	$EscolhaModo.show()
 
-
-
+#Faz o contrario da função "_on_NewGameButton_pressed"
+func _on_VoltarButton_pressed():
+	$MainChoices.show()
+	$EscolhaModo.hide()
