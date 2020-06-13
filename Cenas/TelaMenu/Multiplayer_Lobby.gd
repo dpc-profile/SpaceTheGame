e4e2305dd3db9d7ac66f46e325f148887ab25505
Luigi-guy <63349995+daniel-PCastro@users.noreply.github.com>
2020-06-13 17:10:09 -0300

    Update
    -Tela de Game Over ajeitada.
    -Mini mapa consertado, ele encontra o player sem precisar referenciar o NodePath.
    -ayaya ayaya.

--- Cenas/TelaMenu/Multiplayer_Lobby.gd
+++ Cenas/TelaMenu/Multiplayer_Lobby.gd
@@ -2,18 +2,12 @@ extends Control
 
 
 func _process(delta):
-<<<<<<< HEAD
 	$"HBoxContainer/Time 1/Label1".text = Network.players[0].name + " (Host)"
-=======
-	$"HBoxContainer/Time 1/Label1".text = Network.players[0].name
-	
->>>>>>> Update Multiplayer
 
 
 func _on_RefreshPlayers_timeout():
 	for i in Network.players:
 		print(Network.players[i])
-<<<<<<< HEAD
 		$RichTextLabel.text = Network.players[i].name
 
 
@@ -24,6 +18,3 @@ func _on_ComecarPartida_pressed():
 func _on_Sair_pressed():
 	get_tree().change_scene('res://Cenas/TelaMenu/TitleScream.tscn')
 	Network.MULTIPLAYER_ON = false
-=======
-		$RichTextLabel.text =" " +Network.players[i].name
->>>>>>> Update Multiplayer
