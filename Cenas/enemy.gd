dc671c5c8a9b73fd94c29b209d75770e6e6769c1
Luigi-guy <63349995+daniel-PCastro@users.noreply.github.com>
2020-06-14 18:32:43 -0300

    Update branch Inimigo

--- Cenas/enemy.gd
+++ Cenas/enemy.gd
@@ -8,7 +8,7 @@ var mov = Vector2(0,0)
 
 var minimap_icon = "mob"
 
-func _process(delta):
+func _physics_process(delta):
 	mov.x = speed
 	mov.y = 0
 	look_at(Global.player_global_pos)
@@ -17,10 +17,16 @@ func _process(delta):
 	mov = mov.rotated(rotation-45)
 	mov = move_and_slide(mov)
 	
+	
 func recebendo_dano(damage):
 	$ProgressBar.value -= damage
+<<<<<<< Updated upstream
 	#chama a tela de GAME OVER
 	if($ProgressBar.value == 0):
 		get_tree().change_scene("res://Cenas/Game_over.tscn")
 
 
+=======
+	if($ProgressBar.value <= 0):
+		queue_free()
+>>>>>>> Stashed changes
