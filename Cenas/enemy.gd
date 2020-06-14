08281ab3af31c6c5bf398b5c837d46f7bc69f5f6
Luigi-guy <63349995+daniel-PCastro@users.noreply.github.com>
2020-06-14 19:25:17 -0300

    Wryyyy
    -Sadboy

--- Cenas/enemy.gd
+++ Cenas/enemy.gd
@@ -8,7 +8,7 @@ var mov = Vector2(0,0)
 
 var minimap_icon = "mob"
 
-func _process(delta):
+func _physics_process(delta):
 	mov.x = speed
 	mov.y = 0
 	if Global.player_global_pos != null:
@@ -21,6 +21,16 @@ func _process(delta):
 func recebendo_dano(damage):
 	$ProgressBar.value -= damage
 	if($ProgressBar.value <= 0):
-		queue_free()
+		dead()
 
+func dead():
+	set_physics_process(false)
+	for child in get_children():
+		if child.has_method('hide'):
+			child.hide()
+	$CollisionPolygon2D.disabled = true
+	global_position = Vector2(-1000, 0)
+	#Remover o enemy do minimapa
+#	remove_from_group("mobs")
+#	remove_from_group("minimap_objects")
 
