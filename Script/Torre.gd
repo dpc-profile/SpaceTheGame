65884c0697732ae8c9c8f66d3d755d9e7fc902fe
Luigi-guy <63349995+daniel-PCastro@users.noreply.github.com>
2020-06-17 15:13:47 -0300

    Torre Disable
    Ao atirar 3 vezes na torre ela desabilita e depois de 4 segundos ela reativa.

--- Script/Torre.gd
+++ Script/Torre.gd
@@ -1,33 +1,30 @@
 extends KinematicBody2D
-#class_name Mob
 
 var cena_bala = preload("res://Cenas/tiroTorre.tscn")
 var mov = Vector2(0,0)
 var velx = 60
 var dentro_area = false
 
+var hp = 60
+var disable = false #Se refere a propria torre
+
 var minimap_icon="mob"
 
-func _ready():
-	dentro_area = false
-
 func _process(delta):
-	
-	if Global.player_global_pos != null:
+	if Global.player_global_pos != null and !disable:
 		look_at(Global.player_global_pos)
-
 
 func _on_Timer_timeout():
 	if dentro_area:
 		disparar()
 
-
 func _on_Area2D_body_entered(body):
-	if body.is_in_group("player1"):
+	if body.is_in_group("player1") and !disable:
 		dentro_area = true
 
 func _on_Area2D_body_exited(body):
-	dentro_area  = false
+	if body.is_in_group("player1"):
+		dentro_area  = false
 	
 func disparar():
 	var bala = cena_bala.instance()
@@ -36,5 +33,20 @@ func disparar():
 	get_tree().root.add_child(bala)
 
 func recebendo_dano(damage):
-	return
-	
+	if !disable:
+		hp -= damage
+		if hp <= 0:
+			tower_disable()
+			yield(get_tree().create_timer(4),"timeout")#Um timer de 4 segundos
+			tower_enable()
+		
+func tower_disable():
+	$Sprite.self_modulate = Color.gray
+	dentro_area = false
+	disable = true
+
+func tower_enable():
+	$Sprite.self_modulate = Color(1,1,1,1)
+	dentro_area = true
+	disable = false
+	hp = 60
